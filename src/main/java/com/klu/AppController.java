3699660682acdb11b3988371926d0992c5009b0a
SpoorthyAne <anespoorthy@gmail.com>
2025-08-09 14:27:16 +0530

    first commit

---
+++ src/main/java/com/klu/AppController.java
@@ -0,0 +1,19 @@
+package com.klu;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@CrossOrigin
+public class AppController {
+	
+	@GetMapping("/sub/{a}/{b}")
+	public String add(@PathVariable("a") int a, @PathVariable("b") int b) {
+		
+		return "Substraction of two numbers are:"+ (a-b);
+	}
+
+}
